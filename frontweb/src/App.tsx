2f7c902c8337e2eae368c36867e8c3cd4c36cee8
taylanne5 <alvestaylanne54@gmail.com>
2022-04-01 09:42:33 -0300

    DS-CARROS

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './assets/styles/custom.scss'
+import './App.css';
+
+import BRoutes from 'Routes';
+
+const App = () =>{
+  return (
+    <BRoutes/>
+
+    
+  );
+}
+
+export default App;
---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,44 @@
+import ProductCard from "components/ProductCard";
+import ButtonSearch from "components/ButtonSearch";
+
+function Catalog() {
+  return (
+      <div className="container my-4">
+        <div className="row">
+          <div>
+            <ButtonSearch />
+          </div>
+
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-4">
+          <ProductCard />
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Catalog;
